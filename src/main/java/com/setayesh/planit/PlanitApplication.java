de8102caf79a93a0e6023340141b0ae1156b5322
Sissighn <99798403+Sissighn@users.noreply.github.com>
2026-07-31 14:26:19 +0200

    fix(application): remove console log for API running status

--- src/main/java/com/setayesh/planit/PlanitApplication.java
+++ src/main/java/com/setayesh/planit/PlanitApplication.java
@@ -8,6 +8,5 @@ public class PlanitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlanitApplication.class, args);
-        System.out.println("PlanIt REST API is running at http://localhost:8080");
     }
 }
